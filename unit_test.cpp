c9d9aa19546d7ac82afb82f2b41941a9f22723b2
Justin Gafford <jgaff001@ucr.edu>
2019-06-07 13:06:13 -0700

    Update unit_test.cpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,42 +1,72 @@
-#include "base.hpp"
-#include "base_factory.hpp"
-#include "scifactory.hpp"
-#include "doublefactory.hpp"
-#include "precisionfactory.hpp"
-#include "scirand.hpp"
-#include "precisionrand.hpp"
-#include "doublerand.hpp"
-#include "sciop.hpp"
-#include "precisionop.hpp"
-#include "doubleop.hpp"
-#include "Op.hpp"
-#include "Rand.hpp"
-#include "Mult.hpp"
-#include "Div.hpp"
-#include "Add.hpp"
-#include "Sub.hpp"
-#include "Pow.hpp"
-#include "menu.hpp"
-#include "command.hpp"
+
+#include "compositepattern.h"
+#include "iterator.h"
 #include <iostream>
 #include "gtest/gtest.h"
 #include "visitor.hpp"
-#include "visitor.cpp"
 
-TEST(Tests, Test)  
-{
-    Base* op1 = new Op(13);
-    Base* op2 = new Op(75);
-    Base* add1 = new Add(op1,op2);
-    CountVisitor *a = new CountVisitor();
-    PreorderIterator *b = new PreorderIterator(add1);
-   
-    b->first();
-    while(b->is_done() == false)
-    {
-	    b->next();
-	  }
-   
-    EXPECT_EQ(a->op_count(),2);
-    EXPECT_EQ(a->add_count(), 1);
+TEST(IteratorTests, BinaryIter) {
+  Op* op2 = new Op(69);
+  Op* op1 = new Op(23);
+  Div* div = new Div(op2,op1);
+  Root* root = new Root(div);
+  BinaryIterator* null_itr = new BinaryIterator(root);
+  EXPECT_EQ(null_itr->is_done(),0);
+  null_itr->first();
+  EXPECT_EQ(null_itr->current()->evaluate(),3.0000000);
+  EXPECT_EQ(null_itr->is_done(),0);
+  null_itr->next();
+  EXPECT_EQ(null_itr->is_done(),1);
+
+}
+
+TEST(IteratorTests, UnaryIter) {
+  Op* testOp = new Op(69);
+  Root* root = new Root(testOp);
+  UnaryIterator* null_itr = new UnaryIterator(root);
+  EXPECT_EQ(null_itr->is_done(),1);
+  null_itr->first();
+  EXPECT_EQ(null_itr->current()->stringify(),"69.000000");
+  EXPECT_EQ(null_itr->is_done(),0);
+  null_itr->next();
+  EXPECT_EQ(null_itr->is_done(),1);
+
+}
+
+TEST(IteratorTests, NullIter) {
+  Op* testOp = new Op(69);
+  Root* root = new Root(testOp);
+  NullIterator* null_itr = new NullIterator(root);
+  EXPECT_EQ(null_itr->is_done(),1);
+  null_itr->first();
+  EXPECT_EQ(null_itr->is_done(),1);
+  null_itr->next();
+  EXPECT_EQ(null_itr->is_done(),1);	
+}
+
+TEST(IteratorTests, PreOrderIter) {
+  Op* op3 = new Op(3);
+  Op* op4 = new Op(4);
+  Op* op2 = new Op(2);
+  Add* add = new Add(op3, op4); //7
+  Pow* pow  = new Pow(add,op2); //49
+  Root* root = new Root(pow);
+  PreOrderIterator* pre_itr = new PreOrderIterator(root);
+  pre_itr->first();
+  EXPECT_EQ(pre_itr->current()->stringify(),"3.000000 + 4.000000 ** 2.000000");
+  pre_itr->next();
+  EXPECT_EQ(pre_itr->current()->stringify(),"3.000000 + 4.000000");
+  pre_itr->next();
+  EXPECT_EQ(pre_itr->current()->stringify(),"3.000000");
+  pre_itr->next();
+  EXPECT_EQ(pre_itr->current()->stringify(),"4.000000");
+  pre_itr->next();
+  EXPECT_EQ(pre_itr->current()->stringify(),"2.000000");
+  pre_itr->next();
+  EXPECT_EQ(pre_itr->is_done(),1);
+
+}
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
