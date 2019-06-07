c327427bbf352fd2ac52feb6c7f9013026c45a37
Justin Gafford <jgaff001@ucr.edu>
2019-06-07 14:48:41 -0700

    Update compositepattern.h

--- compositepattern.h
+++ compositepattern.h
@@ -34,7 +34,7 @@ class Op: public Base {
     public:
         Op();
         Op(double val);
-        void accept(CountVisitor*) { visit_op() };
+        void accept(CountVisitor*) { this->visit_op() };
         Base* get_left();
         Base* get_right();
         double evaluate(); 
@@ -48,7 +48,7 @@ class Rand : public Base
         double num;
     public:
         Rand();
-        void accept(CountVisitor*) { visit_rand() };
+        void accept(CountVisitor*) { this->visit_rand() };
         Base* get_left();
         Base* get_right();
         double evaluate();
@@ -88,7 +88,7 @@ class Add: public Operator {
     public:
         Add();
         Add(Base* left, Base* right);
-        void accept(CountVisitor*) { visit_add() };
+        void accept(CountVisitor*) { this->visit_add() };
         string stringify();
         double evaluate();
 };
@@ -97,7 +97,7 @@ class Sub: public Operator {
     public:
         Sub();
         Sub(Base* left, Base* right);
-        void accept(CountVisitor*) { visit_sub() };
+        void accept(CountVisitor*) { this->visit_sub() };
         string stringify();
         double evaluate();
 };
@@ -106,7 +106,7 @@ class Mult: public Operator {
     public:
         Mult();
         Mult(Base* left, Base* right);
-        void accept(CountVisitor*) { visit_mult() };
+        void accept(CountVisitor*) { this->visit_mult() };
         string stringify();
         double evaluate();
 };
@@ -115,7 +115,7 @@ class Div: public Operator {
      public:
         Div();
         Div(Base* left, Base* right);
-        void accept(CountVisitor*) { visit_div() };
+        void accept(CountVisitor*) { this->visit_div() };
         string stringify();
         double evaluate();
 };
@@ -123,7 +123,7 @@ class Pow: public Operator {
     public:
         Pow();
         Pow(Base* left, Base* right);
-        void accept(CountVisitor*) {visit_pow() };
+        void accept(CountVisitor*) {this->visit_pow() };
         string stringify();
         double evaluate();
 };
@@ -152,7 +152,7 @@ class Floor : public UnaryOperator
     public: 
         Floor() : UnaryOperator() {};
         Floor(Base* cory) : UnaryOperator(cory){};
-        void accept(CountVisitor*) { visit_floor(); }
+        void accept(CountVisitor*) { this->visit_floor(); }
         double evaluate();
         string stringify() {};
 };
@@ -162,7 +162,7 @@ class Abs : public UnaryOperator
     public: 
         Abs() : UnaryOperator() {};
         Abs(Base* cory) : UnaryOperator(cory){};
-        void accept(CountVisitor*) { visit_abs(); };
+        void accept(CountVisitor*) { this->visit_abs(); };
         double evaluate();
         string stringify() {};
 };
@@ -172,7 +172,7 @@ class Trunc : public UnaryOperator
     public: 
         Trunc() : UnaryOperator() {};
         Trunc(Base* cory) : UnaryOperator(cory){};
-        void accept(CountVisitor*) {visit_trunc(); };
+        void accept(CountVisitor*) {this->visit_trunc(); };
         string stringify();
         double evaluate() {return child->evaluate();};
 };
@@ -181,7 +181,7 @@ class Paren : public UnaryOperator
 {
     public: 
         Paren(Base* cory) : UnaryOperator(cory){};
-        void accept(CountVisitor*) { visit_paren(); };
+        void accept(CountVisitor*) { this->visit_paren(); };
         string stringify();
         double evaluate() {return child->evaluate();};
 };
